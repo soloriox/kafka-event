708b52f4a9a508819b1735987051a9b6b4ba5e2f
soloriox <eseoeleoereio@gmail.com>
2026-02-14 13:39:33 -0700

    Add Stream Provider to project.

---
+++ SpringBootKafka/SpringBootProvider/src/main/java/com/kafka/provider/SpringBootProviderApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProviderApplication.class, args);
+	}
+
+}
